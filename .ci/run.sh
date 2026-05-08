770d20ceee39bdbda07ab954bda137837a40f064
Jan Kotanski <jankotan@gmail.com>
2026-05-08 20:48:44 +0200

    Issue 30: add ubuntu 26.04  tests (#31)
    * add debian 13 tests
    
    * remove software-properties-common from debian13 doecker
    
    * add debian 13 tests
    
    * add 25.10
    
    * change ub25.10 to ub26.04 tests
    
    * add first test
    
    * add missing file
    
    * add printout
    
    * add printout
    
    * add printout
    
    * add printout

--- .ci/run.sh
+++ .ci/run.sh
@@ -4,9 +4,11 @@ echo "run nxsconfigserver-db"
 if [ $1 = "2" ]; then
     docker exec  ndts python test/runtest.py
 else
-    docker exec  ndts python3 test/runtest.py
-fi    
-if [ $? -ne "0" ]
+    docker exec  ndts python3 -m pytest test
+fi
+ERROR=$?
+if [ $ERROR -ne "0" ]
 then
+    echo "ERROR "$ERROR
     exit 255
 fi
