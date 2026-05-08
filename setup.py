43a0087833cca53875f896dac0a15202866656be
Jan Kotanski <jankotan@gmail.com>
2026-05-08 21:35:28 +0200

    update setup

--- setup.py
+++ setup.py
@@ -76,6 +76,7 @@ SETUPDATA = dict(
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
     ],
     cmdclass={'build_sphinx': BuildDoc},
     packages=[],
